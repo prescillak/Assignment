da10f12da174e28c3fc5d3cdab76e4f8168bd63f
prescillak <prescillakola@gmail.com>
2020-10-09 20:41:56 -0400

    Sec #14

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,8 @@
-import { EventEmitter } from '@angular/core';
+import{Subject} from 'rxjs';
 import{Ingredient} from '../shared/ingredient.model';
 
 export class ShoppingListService{
-    ingredientsChanged=new EventEmitter<Ingredient[]>();
+    ingredientsChanged=new Subject<Ingredient[]>();
   private  ingredients:Ingredient[]=[
         new Ingredient('Apples',5),
         new Ingredient('TOMATOES',10),
@@ -13,10 +13,10 @@ getIngredients(){
 }
 addIngredient(ingredient:Ingredient){
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
 }
 addIngre(ingredients:Ingredient[]){
 this.ingredients.push(...ingredients);   // ... Spread operator
-this.ingredientsChanged.emit(this.ingredients.slice());
+this.ingredientsChanged.next(this.ingredients.slice());
 }
 }
