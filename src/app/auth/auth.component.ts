96498ea38161f9f72a8b59ef48b54d95306fada4
prescillak <prescillakola@gmail.com>
2021-02-20 20:40:20 -0500

    #297-sending login reqsts

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import{Component} from '@angular/core';
 import { NgForm } from '@angular/forms';
-import{AuthService}from'./auth.service';
+import { Observable } from 'rxjs';
+import{AuthService, AuthResponseData}from'./auth.service';
 
 @Component({
     selector:'app-auth',
@@ -24,25 +25,26 @@ export class AuthComponent{
              }
          const email=form.value.email;
          const password=form.value.password;
+    let authObs:Observable<AuthResponseData>
         
          this.isLoading=true;
 
           if(this.isLoginMode){
-            //...
-             }else{
-                     this.authService.signup(email,password).subscribe(
-                          resData=>{
-                    console.log(resData);
-                    this.isLoading=false;
-                        },
-                errorMessage=>{
-                console.log(errorMessage);
-                    this.error=errorMessage;
-                this.isLoading=false;
-            }
-            );
-        }
-
+             authObs= this.authService.login(email,password)
+           }else{
+                authObs= this.authService.signup(email,password);
+                  }
+authObs.subscribe(
+    resData=>{
+        console.log(resData);
+        this.isLoading=false;
+            },
+    errorMessage=>{
+    console.log(errorMessage);
+        this.error=errorMessage;
+    this.isLoading=false;
+}
+); 
         form.reset();
     }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,13 +3,14 @@ import{HttpClient} from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import{throwError} from 'rxjs';
 
-interface AuthResponseData{
+export interface AuthResponseData{
     kind:string;
     idToken:string;
     email:string;
     refreshToken:string;
     expiresIn:string;
     localId:string;
+    registered?:boolean;
 }
 @Injectable({providedIn:'root'})
 export class AuthService{
@@ -35,5 +36,17 @@ export class AuthService{
         })
         );
    
+    }
+
+
+    login(email:string,password:string){
+       return this.http.post<AuthResponseData>(
+           'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDq4J6fxxXY5n0IBaqKYdeEfsZRKecID5w',
+        {
+            email:email,
+            password:password,
+            returnSecureToken: true
+            }
+            );
     }
 }
