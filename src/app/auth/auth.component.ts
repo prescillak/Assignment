3648c8430c536f248235616c3c2730c8efb69e31
prescillak <prescillakola@gmail.com>
2021-02-24 18:10:38 -0500

    #302-attaching token with interceptor

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,6 +2,7 @@ import{Component} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Observable } from 'rxjs';
 import{AuthService, AuthResponseData}from'./auth.service';
+import{Router} from'@angular/router';
 
 @Component({
     selector:'app-auth',
@@ -13,7 +14,7 @@ export class AuthComponent{
     isLoading=false;
     error:string=null;
 
-    constructor(private authService: AuthService){}
+    constructor(private authService: AuthService, private router:Router){}
 
     onSwitchMode(){
         this.isLoginMode=!this.isLoginMode;
@@ -34,10 +35,11 @@ export class AuthComponent{
            }else{
                 authObs= this.authService.signup(email,password);
                   }
-authObs.subscribe(
+ authObs.subscribe(
     resData=>{
         console.log(resData);
         this.isLoading=false;
+        this.router.navigate(['/recipes']);
             },
     errorMessage=>{
     console.log(errorMessage);
