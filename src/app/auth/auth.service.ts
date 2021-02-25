7762ea5fc49cf94218afbb0afe8163cd2bab9188
prescillak <prescillakola@gmail.com>
2021-02-24 23:41:48 -0500

    #304-autologin

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -59,6 +59,28 @@ export class AuthService{
             );
     }
 
+    autoLogin(){
+       const userData:{
+           email:string;
+           id:string;
+           _token:string;
+           _tokenExpirationDate:string;
+       }= JSON.parse(localStorage.getItem('userData'));
+        if(!userData){
+            return;
+        }
+        const loadedUser=new User(
+            userData.email,
+            userData.id,
+            userData._token,
+            new Date(userData._tokenExpirationDate)
+            );
+
+            if(loadedUser.token){
+                this.user.next(loadedUser);
+            }
+    }
+
 logout(){
     this.user.next(null);
     this.router.navigate(['/auth']);
@@ -74,6 +96,7 @@ private handleAuthentication(email:string,userId:string,token:string,expiresIn:n
       token,
         expirationDate);
     this.user.next(user);
+    localStorage.setItem('userData',JSON.stringify(user));
 }
     
     private handleError(errorRes:HttpErrorResponse){
