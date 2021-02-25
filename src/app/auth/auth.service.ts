2566c8a77dc0e9697f90d95e97876ab87a9fb21e
prescillak <prescillakola@gmail.com>
2021-02-24 23:19:29 -0500

    #303-Logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,6 +3,7 @@ import{HttpClient, HttpErrorResponse} from '@angular/common/http';
 import { catchError, tap } from 'rxjs/operators';
 import{BehaviorSubject, throwError} from 'rxjs';
 import{User} from './user.model';
+import { Router } from '@angular/router';
 
 export interface AuthResponseData{
     kind:string;
@@ -18,7 +19,7 @@ export class AuthService{
 
     user=new BehaviorSubject<User>(null);
 
-    constructor(private http:HttpClient){}
+    constructor(private http:HttpClient, private router:Router){}
 
     signup(email:string,password:string){
         return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDq4J6fxxXY5n0IBaqKYdeEfsZRKecID5w',
@@ -57,6 +58,13 @@ export class AuthService{
              })
             );
     }
+
+logout(){
+    this.user.next(null);
+    this.router.navigate(['/auth']);
+}
+  
+    
 private handleAuthentication(email:string,userId:string,token:string,expiresIn:number){
     const expirationDate=new Date(
         new Date().getTime()+ expiresIn*1000);
